8fd89d2750dd1d2a332d6fa69520e49ee2c30f8e
galexy <87016261+oggalexy@users.noreply.github.com>
2026-07-26 01:41:27 +0200

    Update permissions.py

--- permissions.py
+++ permissions.py
@@ -1,1 +1,12 @@
+import discord
 
+async def is_admin(interaction):
+
+    if not interaction.user.guild_permissions.administrator:
+        await interaction.response.send_message(
+            "❌ Šią komandą gali naudoti tik administratoriai.",
+            ephemeral=True
+        )
+        return False
+
+    return True
