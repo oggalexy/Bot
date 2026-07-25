054ae23385b9dddc00e27594659f3c2b3df95a86
galexy <87016261+oggalexy@users.noreply.github.com>
2026-07-26 01:53:38 +0200

    Update embeds.py

--- embeds.py
+++ embeds.py
@@ -1,31 +1,62 @@
 import discord
 
 
-def account_embed(type, accounts, user):
+def account_embed(account_type, accounts, user):
 
     embed = discord.Embed(
-        title=f"🟢 {type.upper()} paskyros",
-        description="Paskyros išduotos sėkmingai.",
+        title=f"🎁 {account_type.upper()} ACCOUNTS",
+        description="✅ Paskyros išduotos",
         color=discord.Color.green()
     )
-
 
     for i, acc in enumerate(accounts, 1):
 
         embed.add_field(
-            name=f"🔹 Paskyra #{i}",
+            name=f"🔹 Account #{i}",
             value=(
                 f"📧 Login:\n"
-                f"`{acc[1]}`\n\n"
+                f"```{acc[1]}```\n"
                 f"🔑 Password:\n"
-                f"`{acc[2]}`"
+                f"```{acc[2]}```"
             ),
             inline=False
         )
 
+    embed.set_footer(
+        text=f"Requested by {user.name}"
+    )
+
+    return embed
+
+
+
+def stock_embed(fivem, discord_acc, steam):
+
+    embed = discord.Embed(
+        title="📦 LIVE ACCOUNT STOCK",
+        color=discord.Color.blue()
+    )
+
+    embed.add_field(
+        name="🎮 FiveM",
+        value=f"```{fivem} available```",
+        inline=True
+    )
+
+    embed.add_field(
+        name="💬 Discord",
+        value=f"```{discord_acc} available```",
+        inline=True
+    )
+
+    embed.add_field(
+        name="🚂 Steam",
+        value=f"```{steam} available```",
+        inline=True
+    )
 
     embed.set_footer(
-        text=f"Pasiėmė: {user}"
+        text="Auto updating every 30 seconds"
     )
 
     return embed
